e910f7896986475193a760a81bbeeb2360546f51
Jonathan Mullins <mondojono@gmail.com>
2016-05-08 13:45:46 +1000

    added css reset and cleaned up a little

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,42 @@
-var context;
-var bufferLoader;
-var bufferList;
+(function() {
 
-$(document).ready(init);
+  var context;
+  var bufferLoader;
+  var bufferList;
 
-// http://www.html5rocks.com/en/tutorials/webaudio/intro/
-function init() {
+  $(document).ready(init);
 
-  var $pads = $(".sampler .pad");
-  var paths = [];
-  $pads.each(function(idx, el) {
-    var $el = $(el);
-    paths.push($el.attr("data-src"));
-    $el.on("click touchstart", function(ev) {
-      ev.preventDefault();
-      playSample(idx);
+  // http://www.html5rocks.com/en/tutorials/webaudio/intro/
+  function init() {
+
+    var $pads = $(".sampler .pad");
+    var paths = [];
+    $pads.each(function(idx, el) {
+      var $el = $(el);
+      paths.push($el.attr("data-src"));
+      $el.on("click touchstart", function(ev) {
+        ev.preventDefault();
+        playSample(idx);
+      });
     });
-  });
 
-  // Fix up prefixing
-  window.AudioContext = window.AudioContext || window.webkitAudioContext;
-  context = new AudioContext();
-  bufferLoader = new BufferLoader(context, paths, function(bl) {
-    bufferList = bl;
-  });
-  bufferLoader.load();
-}
+    // Fix up prefixing
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
-function playSample(num) {
-  // Create two sources and play them both together.
-  var source = context.createBufferSource();
-  source.buffer = bufferList[num];
-  source.connect(context.destination);
-  source.start(0);
-}
+    context = new AudioContext();
+    bufferLoader = new BufferLoader(context, paths, function(bl) {
+      bufferList = bl;
+    });
+    bufferLoader.load();
+  }
+
+  function playSample(num) {
+    // Create two sources and play them both together.
+    var source = context.createBufferSource();
+    source.buffer = bufferList[num];
+    source.connect(context.destination);
+    source.start(0);
+  }
+
+})();
 
